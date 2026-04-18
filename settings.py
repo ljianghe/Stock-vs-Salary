db3e1c33fe5763fa033f6e94e500a4f31bb49015
Lily Jiang He <ljianghe@iu.edu>
2026-04-18 15:32:01 -0400

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,23 +1,51 @@
 from os import environ
-SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=1.0, participation_fee=0.0)
-SESSION_CONFIGS = [dict(name='public_goods_leader', num_demo_participants=5, app_sequence=['public_goods_leader']), dict(name='public_goods_variable_leader', num_demo_participants=5, app_sequence=['public_goods_leader_variable'])]
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.0,
+    participation_fee=0.0,
+)
+
+SESSION_CONFIGS = [
+    dict(
+        name='public_goods_leader',
+        num_demo_participants=5,
+        app_sequence=['public_goods_leader'],
+    ),
+    dict(
+        name='public_goods_variable_leader',
+        num_demo_participants=5,
+        app_sequence=['public_goods_leader_variable'],
+    ),
+]
+
 LANGUAGE_CODE = 'en'
 REAL_WORLD_CURRENCY_CODE = 'USD'
 USE_POINTS = True
 DEMO_PAGE_INTRO_HTML = ''
-PARTICIPANT_FIELDS = ['prolific_id']
-SESSION_FIELDS = []
+
+# settings.py
+PARTICIPANT_FIELDS = [
+    'prolific_id',
+    'is_dropout',
+    'needs_dropout_notice',
+    'contrib_done',
+    'trust_done',
+    'round_ready',
+]
+
+SESSION_FIELDS = [
+    'leader_left',
+    'last_leader_message',
+    'member_left',
+]
+
 THOUSAND_SEPARATOR = ''
 AUTO_TABULATE_PAYOFFS = False
 ROOMS = []
 
 ADMIN_USERNAME = 'admin'
-# for security, best to set admin password in an environment variable
 ADMIN_PASSWORD = '1234'
 
 SECRET_KEY = 'blahblah'
 
-# if an app is included in SESSION_CONFIGS, you don't need to list it here
 INSTALLED_APPS = ['otree']
-
-
